5a9a5833066f8890ab3b83a0c3ad6c99282f2487
raif <rraiffahmi@gmail.com>
2024-01-29 11:08:25 +0700

    error

--- database/migrations/2024_01_29_034250_create_product_table.php
+++ database/migrations/2024_01_29_034250_create_product_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('price');
             $table->integer('qty');
             $table->string('material', 255);
+            $table->string('img_name', 255);
             $table->timestamps();
         });
     }
